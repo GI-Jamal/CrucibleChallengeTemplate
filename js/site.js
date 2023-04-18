26be5f0a867e05d362a5ff6aa2dbc2984053deb4
Jamal Gibson <jamal.gibson@rogers.com>
2023-04-18 18:07:21 -0400

    Commit msg

--- js/site.js
+++ js/site.js
@@ -1,12 +1,17 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
 function displayMessage() {
-  let msg = document.getElementById('message').value;
+  let msg = document.getElementById("message").value;
   //alert(msg);
 
-  Swal.fire(
-    {
-        backdrop: false,
-        title: 'App Name',
-        text: msg
-    }
-  );
+  Swal.fire({
+    backdrop: false,
+    title: "App Name",
+    text: msg,
+  });
 }
